6a5e5e36d17ef32fdd54408669f3ed8e19a4f1e3
Jomary Coquilla <jomarycoquilla465@gmail.com>
2025-07-29 07:37:30 +0800

    .

--- cnu-police/next.config.ts
+++ cnu-police/next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Bypass ESLint during build
   },
-  typescript: {
-    ignoreBuildErrors: true, // Bypass TypeScript errors during build
-  },
 };
 
 export default nextConfig;
